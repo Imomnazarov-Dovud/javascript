3fa10730ed6ce6d07c40e2192ed6b2249f7a091b
Imomnazarov <you@example.com>
2022-06-05 15:17:58 +0300

    Tetris

---
+++ functiones.js
@@ -0,0 +1,65 @@
+var first = function () {
+   var block = new Block(20, 1)
+   var block1 = new Block(20, 2)
+   var block2 = new Block(20, 3)
+   var block3 = new Block(19, 1)
+   block.drawSquare()
+   block1.drawSquare()
+   block2.drawSquare()
+   block3.drawSquare()
+}
+
+var second = function () {
+   var block = new Block(20, 1)
+   var block1 = new Block(20, 2)
+   var block2 = new Block(20, 3)
+   var block3 = new Block(20, 4)
+   block.drawSquare()
+   block1.drawSquare()
+   block2.drawSquare()
+   block3.drawSquare()
+}
+
+var third = function () {
+   var block = new Block(20, 1)
+   var block1 = new Block(20, 2)
+   var block2 = new Block(20, 3)
+   var block3 = new Block(21, 1)
+   block.drawSquare()
+   block1.drawSquare()
+   block2.drawSquare()
+   block3.drawSquare()
+}
+
+var forth = function () {
+   var block = new Block(20, 1)
+   var block1 = new Block(20, 2)
+   var block2 = new Block(19, 2)
+   var block3 = new Block(19, 1)
+   block.drawSquare()
+   block1.drawSquare()
+   block2.drawSquare()
+   block3.drawSquare()
+}
+
+var fifeth = function () {
+   var block = new Block(20, 1)
+   var block1 = new Block(20, 2)
+   var block2 = new Block(19, 2)
+   var block3 = new Block(19, 3)
+   block.drawSquare()
+   block1.drawSquare()
+   block2.drawSquare()
+   block3.drawSquare()
+}
+
+var sixth = function () {
+   var block = new Block(20, 1)
+   var block1 = new Block(20, 2)
+   var block2 = new Block(21, 2)
+   var block3 = new Block(19, 3)
+   block.drawSquare()
+   block1.drawSquare()
+   block2.drawSquare()
+   block3.drawSquare()
+}
