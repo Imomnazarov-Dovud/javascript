ff3d12e3c754034e825344f730d22f03a107da6f
Imomnazarov <you@example.com>
2022-06-05 16:44:32 +0300

    Last version

--- functiones.js
+++ functiones.js
@@ -9,6 +9,39 @@ var first = function () {
    block3.drawSquare()
 }
 
+var first1 = function () {
+   var block = new Block(20, 1)
+   var block1 = new Block(19, 1)
+   var block2 = new Block(19, 2)
+   var block3 = new Block(21, 1)
+   block.drawSquare()
+   block1.drawSquare()
+   block2.drawSquare()
+   block3.drawSquare()
+}
+
+var first2 = function () {
+   var block = new Block(20, 1)
+   var block1 = new Block(20, 2)
+   var block2 = new Block(20, 3)
+   var block3 = new Block(21, 3)
+   block.drawSquare()
+   block1.drawSquare()
+   block2.drawSquare()
+   block3.drawSquare()
+}
+
+var first3 = function () {
+   var block = new Block(21, 1)
+   var block1 = new Block(21, 2)
+   var block2 = new Block(20, 2)
+   var block3 = new Block(19, 2)
+   block.drawSquare()
+   block1.drawSquare()
+   block2.drawSquare()
+   block3.drawSquare()
+}
+
 var second = function () {
    var block = new Block(20, 1)
    var block1 = new Block(20, 2)
@@ -20,11 +53,55 @@ var second = function () {
    block3.drawSquare()
 }
 
+var second1 = function () {
+   var block = new Block(20, 1)
+   var block1 = new Block(19, 1)
+   var block2 = new Block(18, 1)
+   var block3 = new Block(20, 1)
+   block.drawSquare()
+   block1.drawSquare()
+   block2.drawSquare()
+   block3.drawSquare()
+}
+
 var third = function () {
    var block = new Block(20, 1)
    var block1 = new Block(20, 2)
    var block2 = new Block(20, 3)
    var block3 = new Block(21, 1)
+   block.drawSquare()
+   block1.drawSquare()
+   block2.drawSquare()
+   block3.drawSquare()
+}
+
+var third1 = function () {
+   var block = new Block(21, 1)
+   var block1 = new Block(21, 2)
+   var block2 = new Block(20, 2)
+   var block3 = new Block(19, 2)
+   block.drawSquare()
+   block1.drawSquare()
+   block2.drawSquare()
+   block3.drawSquare()
+}
+
+var third2 = function () {
+   var block = new Block(20, 1)
+   var block1 = new Block(21, 1)
+   var block2 = new Block(21, 2)
+   var block3 = new Block(21, 3)
+   block.drawSquare()
+   block1.drawSquare()
+   block2.drawSquare()
+   block3.drawSquare()
+}
+
+var third3 = function () {
+   var block = new Block(20, 1)
+   var block1 = new Block(21, 1)
+   var block2 = new Block(22, 1)
+   var block3 = new Block(20, 2)
    block.drawSquare()
    block1.drawSquare()
    block2.drawSquare()
